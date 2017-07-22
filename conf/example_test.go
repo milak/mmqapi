00012982e231ae1d8cfd582ef118f05e4882fdfc
milak <milak@milak>
2017-07-22 21:09:55 +0200

    MINOR

---
+++ conf/example_test.go
@@ -0,0 +1,11 @@
+package conf
+
+import (
+
+)
+
+func ExampleParameter() {
+	var p Parameter
+	p.Name = "key"
+	p.Value = "value"
+}
--- useless/test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"fmt"
-)
-
-func main(){
-	fmt.Println("Unnecessary main func to avoid 'no buildable Go source files'")
-}
