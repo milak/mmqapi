610c9e8303f71ee11bce8d18b56d6469bca755e6
milak <milak@milak>
2017-07-22 19:52:46 +0200

    Use of GetName() in service

--- env/context.go
+++ env/context.go
@@ -46,6 +46,6 @@ func NewContext(conf *conf.Configuration) *Context {
 	return &Context{Running : true, Configuration : conf, Logger : logger, Host : host, InstanceName : InstanceName, Services : make([]Service,0)}
 }
 func (this *Context) RegisterService(aService Service){
-	this.Logger.Println("DEBUG Service UNKNOWN registered")
+	this.Logger.Println("DEBUG Service '"+aService.GetName()+"' registered")
 	this.Services = append(this.Services,aService)
 }
